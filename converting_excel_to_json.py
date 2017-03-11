0c85daad1d39d235ebde2a394424c48ea8b91cd7
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-11 13:03:50 -0300

    converting csv file to json file

---
+++ converting_excel_to_json.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+msft= pd.read_csv("sample.csv")   #creating an object to read the excel sheet
+#print(msft.head())                # to print the excel sheet
+
+#df=msft.to_json("sample.json")    #to convert it to json file
+
+df=pd.read_json("sample.json")    #to read json file
+
+print(df.head())                   #print json file
